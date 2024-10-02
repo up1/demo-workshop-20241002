6b9460a039cd95885e264913e3f481a8dfd4fbd1
up1 <somkiat.p@gmail.com>
2024-10-02 13:36:56 +0700

    Create user controller

---
+++ day1/src/main/java/com/example/day1/user/UserResponse.java
@@ -0,0 +1,42 @@
+package com.example.day1.user;
+
+public class UserResponse{
+	private int id;
+	private String fname;
+	private String lname;
+
+
+	public void setFname(String fname){
+		this.fname = fname;
+	}
+
+	public String getFname(){
+		return fname;
+	}
+
+	public void setLname(String lname){
+		this.lname = lname;
+	}
+
+	public String getLname(){
+		return lname;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"UserResponse{" + 
+			"fname = '" + fname + '\'' + 
+			",lname = '" + lname + '\'' + 
+			",id = '" + id + '\'' + 
+			"}";
+		}
+}
