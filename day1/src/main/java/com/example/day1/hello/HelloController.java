d4546a54f1cedeb27841a6e1f76c8565cff059df
up1 <somkiat.p@gmail.com>
2024-10-02 11:03:45 +0700

    Hello spring boot

---
+++ day1/src/main/java/com/example/day1/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.day1.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Spring boot";
+    }
+
+}
