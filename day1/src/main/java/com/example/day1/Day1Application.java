9abd54af6e4d7efa792afd4e3e1e67700a1784f1
up1 <somkiat.p@gmail.com>
2024-10-02 11:30:00 +0700

    Count number of beans

--- day1/src/main/java/com/example/day1/Day1Application.java
+++ day1/src/main/java/com/example/day1/Day1Application.java
@@ -2,12 +2,15 @@ package com.example.day1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Day1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day1Application.class, args);
+		ConfigurableApplicationContext context =
+				SpringApplication.run(Day1Application.class, args);
+		System.out.println(context.getBeanDefinitionCount());
 	}
 
 }
