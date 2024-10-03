f88e309f0c129d8bfa17c4dc4726ab2a62ba08b7
up1 <somkiat.p@gmail.com>
2024-10-03 11:32:26 +0700

    Integrate service + repository

--- day1/src/main/java/com/example/day1/user/UserService.java
+++ day1/src/main/java/com/example/day1/user/UserService.java
@@ -1,17 +1,25 @@
 package com.example.day1.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     public UserResponse get(int id) {
-        if(id == 2) {
+        Optional<MyUser> result =  userRepository.findById(1L);
+        if (result.isEmpty()) {
             throw new UserNotFoundException("User id =" + id + " not found");
         }
         UserResponse userResponse = new UserResponse();
         userResponse.setId(id);
-        userResponse.setFname("Somkiat");
-        userResponse.setLname("Pui");
+        userResponse.setFname(result.get().getFirstName());
+        userResponse.setLname(result.get().getLastName());
         return userResponse;
     }
 }
---
+++ day1/src/test/java/com/example/day1/user/UserRepositoryMySQLTest.java
@@ -0,0 +1,33 @@
+package com.example.day1.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class UserRepositoryMySQLTest {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql");
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> mySQLContainer.getJdbcUrl());
+        registry.add("spring.datasource.driverClassName", () -> mySQLContainer.getDriverClassName());
+        registry.add("spring.datasource.username", () -> mySQLContainer.getUsername());
+        registry.add("spring.datasource.password", () -> mySQLContainer.getPassword());
+    }
+
+    @Test
+    public void case01(){
+
+    }
+
+}
