8d56190d4725703072320b49879008647a477f25
up1 <somkiat.p@gmail.com>
2024-10-03 10:19:16 +0700

    Service testing with SpringBootTest

---
+++ day1/src/test/java/com/example/day1/user/UserServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.day1.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @DisplayName("Success case")
+    void case01() {
+        // Act
+        UserResponse result = userService.get(1);
+        // Assert
+        assertEquals(1, result.getId());
+        assertEquals("Somkiat", result.getFname());
+        assertEquals("Pui", result.getLname());
+    }
+
+    @Test
+    @DisplayName("User not found case")
+    void case02() {
+        Exception exception = assertThrows(UserNotFoundException.class, ()
+                -> userService.get(2));
+        assertEquals("User id =2 not found", exception.getMessage());
+    }
+}
