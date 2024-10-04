aaf21e56eb7065ac54407f26301d1d7bd80b1d7c
up1 <somkiat.p@gmail.com>
2024-10-04 14:00:50 +0700

    Create /mypost/id

--- day1/src/main/java/com/example/day1/user/UserController.java
+++ day1/src/main/java/com/example/day1/user/UserController.java
@@ -1,15 +1,33 @@
 package com.example.day1.user;
 
+import com.example.day1.post.PostGateway;
+import com.example.day1.post.PostResponse;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
-
     private final UserService userService;
+    private final PostGateway postGateway;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PostGateway postGateway) {
         this.userService = userService;
+        this.postGateway = postGateway;
+    }
+
+    @GetMapping("/mypost/{id}")
+    public UserResponse workWithPost(@PathVariable int id){
+        Optional<PostResponse> result = postGateway.getById(id);
+        if(result.isEmpty()) {
+            // Empty
+        }
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(result.get().getId());
+        userResponse.setFname(result.get().getBody().toUpperCase());
+        return userResponse;
     }
 
     @GetMapping("/user/{id}")
